a49a755dc6feead986a994b0d7a19564ad04d523
krish-1010 <mkrishna.inbox@gmail.com>
2025-12-26 09:00:23 +0530

    fixes for login - force https url

--- src/app.js
+++ src/app.js
@@ -14,6 +14,7 @@ const authRoutes = require("./routes/authRoutes");
 const financeRoutes = require("./routes/financeRoutes");
 
 const app = express();
+app.set('trust proxy', 1);
 
 // 1. Database Connection
 mongoose
--- src/config/passport.js
+++ src/config/passport.js
@@ -7,7 +7,9 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/api/auth/google/callback",
+      // callbackURL: "/api/auth/google/callback",
+      callbackURL:
+        "https://finance-backend-ww5g.onrender.com/api/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
